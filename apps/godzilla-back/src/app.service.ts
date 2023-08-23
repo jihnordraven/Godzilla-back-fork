4030557e32cacc3ea9e45a2a0064c198edaeb68c
dimaPolonez <dima-barxat@yandex.ru>
2023-08-23 21:21:44 +0300

    new deploy

--- apps/godzilla-back/src/app.service.ts
+++ apps/godzilla-back/src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('Server is start');
     return `Hello World! ${process.env.PORT}`;
   }
 }
