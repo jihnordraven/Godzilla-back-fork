ed768a8d0fbf749a3ef166331d94a19246b44c0b
dimaPolonez <dima-barxat@yandex.ru>
2023-09-02 16:47:36 +0300

    fix bags to deploy swagger

--- apps/godzilla-back/src/main.ts
+++ apps/godzilla-back/src/main.ts
@@ -28,7 +28,14 @@ async function appLoader() {
   if (CONFIG.DEPLOY === 'TEST') {
     const options = swaggerConfig.development;
     const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api/v1/testing', app, document);
+    SwaggerModule.setup('api/v1/testing', app, document, {
+      customCssUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+      ],
+    });
   }
 
   await app.listen(CONFIG.PORT);
