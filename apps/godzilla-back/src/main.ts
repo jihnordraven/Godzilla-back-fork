735f7f455bc881463643223ea4acf12d37555efa
= <=>
2023-09-08 00:32:21 +0300

    refactor-main

--- apps/godzilla-back/src/main.ts
+++ apps/godzilla-back/src/main.ts
@@ -13,43 +13,83 @@ import { blue, red } from "colorette"
 import { validatePipeOptions } from "../../../libs/errors-handlers"
 import { swaggerSetup } from "../../../libs/swagger/swagger.setup"
 
-async function appLoader() {
+// async function appLoader() {
+// 	const logger: Logger = new Logger("bootstrap")
+
+// 	const app = await NestFactory.create<INestApplication>(AppModule)
+
+// 	app.enableCors({
+// 		origin: [
+// 			"http://localhost:3000",
+// 			"https://godzilla-front.vercel.app/",
+// 			"https://godzillagram.com/"
+// 		],
+// 		credentials: true,
+// 		methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+// 	})
+
+// 	app.use(cookieParser())
+
+// 	useContainer(app.select(AppModule), { fallbackOnErrors: true })
+// 	app.useGlobalPipes(new ValidationPipe(validatePipeOptions))
+// 	// app.useGlobalFilters(new HttpExceptionFilter())
+
+// 	app.setGlobalPrefix("api/v1")
+
+// 	const config = app.get(ConfigService) as ConfigService
+// 	const DEPLOY: string = config.get<string>("DEPLOY")
+// 	const PORT: number = config.get<number>("PORT")
+
+// 	if (DEPLOY === "TEST") swaggerSetup(app)
+
+// 	await app.listen(PORT)
+// 	logger.log(blue(`Server is running on ${PORT} with status: ${DEPLOY}`))
+// }
+
+// async function bootstrap(): Promise<void> {
+// 	const logger: Logger = new Logger("bootstrap")
+// 	try {
+// 		await appLoader()
+// 	} catch (err: unknown) {
+// 		logger.error(red(`Unable to launch server. Learn more at: ${err}`))
+// 		throw new InternalServerErrorException()
+// 	}
+// }
+
+// bootstrap()
+
+const bootstrap = async (): Promise<void> => {
 	const logger: Logger = new Logger("bootstrap")
 
-	const app = await NestFactory.create<INestApplication>(AppModule)
-
-	app.enableCors({
-		origin: [
-			"http://localhost:3000",
-			"https://godzilla-front.vercel.app/",
-			"https://godzillagram.com/"
-		],
-		credentials: true,
-		methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
-	})
-
-	app.use(cookieParser())
-
-	useContainer(app.select(AppModule), { fallbackOnErrors: true })
-	app.useGlobalPipes(new ValidationPipe(validatePipeOptions))
-	// app.useGlobalFilters(new HttpExceptionFilter())
-
-	app.setGlobalPrefix("api/v1")
-
-	const config = app.get(ConfigService) as ConfigService
-	const DEPLOY: string = config.get<string>("DEPLOY")
-	const PORT: number = config.get<number>("PORT")
-
-	if (DEPLOY === "TEST") swaggerSetup(app)
-
-	await app.listen(PORT)
-	logger.log(blue(`Server is running on ${PORT} with status: ${DEPLOY}`))
-}
-
-async function bootstrap(): Promise<void> {
-	const logger: Logger = new Logger("bootstrap")
 	try {
-		await appLoader()
+		const app = await NestFactory.create<INestApplication>(AppModule)
+
+		app.enableCors({
+			origin: [
+				"http://localhost:3000",
+				"https://godzilla-front.vercel.app/",
+				"https://godzillagram.com/"
+			],
+			credentials: true,
+			methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+		})
+
+		app.use(cookieParser())
+
+		useContainer(app.select(AppModule), { fallbackOnErrors: true })
+		app.useGlobalPipes(new ValidationPipe(validatePipeOptions))
+		// app.useGlobalFilters(new HttpExceptionFilter())
+
+		app.setGlobalPrefix("api/v1")
+
+		const config = app.get(ConfigService) as ConfigService
+		const DEPLOY: string = config.get<string>("DEPLOY")
+		const PORT: number = config.get<number>("PORT")
+
+		if (DEPLOY === "TEST") swaggerSetup(app)
+
+		await app.listen(PORT)
+		logger.log(blue(`Server is running on ${PORT} with status: ${DEPLOY}`))
 	} catch (err: unknown) {
 		logger.error(red(`Unable to launch server. Learn more at: ${err}`))
 		throw new InternalServerErrorException()
